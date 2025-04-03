821403d03410ed35218333eafdfcc6d754bdc1d5
Jeevashreeplotch <142967135+Jeevashreeplotch@users.noreply.github.com>
2025-04-03 15:32:30 +0530

    url added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://task-management-krsf.vercel.app";
 
 export const registerUser = (userData) => axios.post(`${API_URL}/register`, userData);
 
